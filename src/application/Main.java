d6ec3967f07e1cbaf249e7a8ed4f25fd3342ddea
Matheus Costa <matetogamesnew@gmail.com>
2023-10-12 03:02:38 -0300

    Initial commit

---
+++ src/application/Main.java
@@ -0,0 +1,30 @@
+package application;
+
+//region IMPORTS
+
+import java.util.Scanner;
+import java.util.Locale;
+
+//endregion
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        //region INSTANCIANDO
+
+        Locale.setDefault(Locale.US);
+        Scanner input = new Scanner(System.in);
+
+        //endregion
+
+
+
+
+        //region FINALIZANDO
+
+        input.close();
+
+        //endregion
+    }
+}
